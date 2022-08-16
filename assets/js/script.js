2257ad3f5069786906268d1a1c88d3933ffe55c3
gabripalmyro <gabripalmyro13579@gmail.com>
2022-08-16 08:39:41 -0300

    Change Icons - Add Fade In Animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
- 
-/* Toggle mobile menu */
-function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-         
-        // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
-    } else {
-        menu.classList.add("active");
-         
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
+// const toggle = document.querySelector(".toggle");
+// const menu = document.querySelector(".menu");
+
+// /* Toggle mobile menu */
+// function toggleMenu() {
+//     if (menu.classList.contains("active")) {
+//         menu.classList.remove("active");
+
+//         // adds the menu (hamburger) icon
+//         toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
+//     } else {
+//         menu.classList.add("active");
+
+//         // adds the close (x) icon
+//         toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
+//     }
+// }
+
+// /* Event Listener */
+// toggle.addEventListener("click", toggleMenu, false);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+        //  else {
+            // reveals[i].classList.remove("active");
+        // }
     }
 }
- 
-/* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
